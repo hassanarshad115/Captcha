ba9f8a09c7cf91272dac1a28bb7e54e9baa55a62
Hassan <Hassan@DESKTOP-MH5E1K5>
2018-07-09 17:14:44 +0500

    Captcha in C#

--- Captcha/Form1.Designer.cs
+++ Captcha/Form1.Designer.cs
@@ -59,7 +59,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(98, 168);
+            this.textBox1.Location = new System.Drawing.Point(97, 168);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
